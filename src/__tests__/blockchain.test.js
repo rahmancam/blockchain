aeb363c95d49952ffc4156f8e27a21c04afc3cd1
Abdul Rahman <rahmancam@live.com>
2021-01-23 18:39:56 +0530

    Add Hash Block and Proof of Work

--- src/__tests__/blockchain.test.js
+++ src/__tests__/blockchain.test.js
@@ -1,4 +1,3 @@
-import Block from '../block';
 import BlockChain from '../blockchain';
 
 describe('Blockchain', () => {
@@ -13,15 +12,55 @@ describe('Blockchain', () => {
     })
 
     test('Create new Block in bitcoin', () => {
-        const newBlock = bitcoin.createNewBlock({
-            nonce: 9802,
-            previousBlockHash: '0xd232DABY5',
-            hash: '0xD4532CDY'
-        })
+        const newBlock = bitcoin.createNewBlock({ nonce: 9802, previousBlockHash: '0xd232DABY5', hash: '0xD4532CDY' })
         expect(bitcoin.chain.length).toEqual(1);
         expect(bitcoin.chain[0].timestamp).not.toBeNull();
         expect(bitcoin.chain[0].previousBlockHash).toEqual(newBlock.previousBlockHash);
 
+    })
+
+    test('Create new Block in bitcoin', () => {
+        bitcoin.createNewBlock({ nonce: 9802, previousBlockHash: '0xd232DABY5', hash: '0xD4532CDY' })
+        const lastBlock = bitcoin.getLastBlock();
+        expect(lastBlock.hash).toEqual('0xD4532CDY');
+    })
+
+    test('Create new transactions in bitcoin', () => {
+        bitcoin.createNewTransaction({ amount: 100, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        expect(bitcoin.pendingTransactions.length).toEqual(1);
+    })
+
+    test('Create new transaction and mine a block', () => {
+        bitcoin.createNewTransaction({ amount: 100, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        bitcoin.createNewTransaction({ amount: 50, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        const blockId = bitcoin.createNewTransaction({ amount: 200, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        bitcoin.createNewBlock({ nonce: 9802, previousBlockHash: '0xd232DABY5', hash: '0xD4532CDY' })
+
+        expect(bitcoin.getLastBlock().index).toEqual(blockId);
+        expect(bitcoin.getLastBlock().transactions.length).toEqual(3);
+    })
+
+    test('hash block', () => {
+        bitcoin.createNewTransaction({ amount: 100, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        bitcoin.createNewTransaction({ amount: 50, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        bitcoin.createNewTransaction({ amount: 200, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+
+        const hash = bitcoin.hashBlock({ nonce: 9802, previousBlockHash: '0xd232DABY5', currentBlockData: bitcoin.pendingTransactions })
+        expect(hash).toEqual('cab06f65e386b57a75022544b9c443aec9ed61a3710fbe569ead759fec885dea');
+    })
+
+    test('Proof of work', () => {
+
+        bitcoin.createNewTransaction({ amount: 100, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        bitcoin.createNewBlock({ nonce: 9802, previousBlockHash: '0xd232DABY5', hash: '0x000D4532CDY' })
+
+        bitcoin.createNewTransaction({ amount: 200, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+        bitcoin.createNewTransaction({ amount: 50, sender: '0xdgu78ddidfnilo69b000b', receiver: '0xegu259khko69b00x' })
+
+        const previousBlockHash = bitcoin.getLastBlock().previousBlockHash;
+        const proofNonce = bitcoin.proofOfWork({ previousBlockHash, currentBlockData: bitcoin.pendingTransactions });
+        const hash = bitcoin.hashBlock({ nonce: proofNonce, previousBlockHash, currentBlockData: bitcoin.pendingTransactions })
+        expect(hash.toString().substring(0, 4)).toEqual('0000');
     })
 })
 
--- src/blockchain.js
+++ src/blockchain.js
@@ -1,9 +1,10 @@
 import Block from './block';
+import sha256 from 'sha256';
 
 class BlockChain {
     constructor() {
         this.chain = [];
-        this.newTransactions = [];
+        this.pendingTransactions = [];
     }
 
     createNewBlock({ nonce, previousBlockHash, hash }) {
@@ -12,14 +13,48 @@ class BlockChain {
             nonce,
             previousBlockHash,
             hash,
-            transactions: this.newTransactions
+            transactions: this.pendingTransactions
         })
 
         this.chain.push(newBlock);
 
-        this.newTransactions = []; // clear for new transactions
+        this.pendingTransactions = []; // clear for new transactions
 
         return newBlock;
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    createNewTransaction({ amount, sender, receiver }) {
+        const newTransaction = {
+            amount,
+            sender,
+            receiver
+        };
+        this.pendingTransactions.push(newTransaction);
+        return this.chain.length + 1; // to which block the transaction going to belongs to
+    }
+
+    hashBlock({ previousBlockHash, currentBlockData, nonce }) {
+        const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+        const hash = sha256(dataAsString);
+        return hash;
+    }
+
+    proofOfWork({ previousBlockHash, currentBlockData }) {
+        // -> repeatedly hash block until it finds correct hash => '0000------------'
+        // -> uses current block data for the hash + previous block hash
+        // -> continuously change the nonce until it find the correct hash
+        // -> returns to us the nonce value that creates the correct hash
+        let nonce = 0;
+        let hash = this.hashBlock({ previousBlockHash, currentBlockData, nonce });
+        while (hash.substring(0, 4) !== '0000') {
+            nonce++;
+            hash = this.hashBlock({ previousBlockHash, currentBlockData, nonce });
+        }
+        return nonce;
     }
 }
 
